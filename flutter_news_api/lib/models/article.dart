45dbd5684b8a42755e67f7a0746bf80aec9f72a6
Alex Pshenychnyi <afw@afw.net.ua>
2021-01-19 21:45:49 +0100

    basic fetch data

---
+++ flutter_news_api/lib/models/article.dart
@@ -0,0 +1,31 @@
+class Article {
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final DateTime publishedAt;
+  final String content;
+
+  Article({
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  factory Article.fromJson(Map<String, Object> json) {
+    return Article(
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: DateTime.parse(json['publishedAt']),
+      content: json['content'],
+    );
+  }
+}
--- flutter_news_api/lib/views/homepage.dart
+++ flutter_news_api/lib/views/homepage.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_news_api/models/article.dart';
+import 'package:flutter_news_api/secret.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,14 +12,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<List<Article>> futureArticles;
+
+  @override
+  void initState() {
+    super.initState();
+    futureArticles = fetchArticles();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: CircularProgressIndicator(),
-        ),
+            child: FutureBuilder<List<Article>>(
+          future: futureArticles,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Text(snapshot.data[index].title),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        )),
       ),
     );
+  }
+}
+
+Future<List<Article>> fetchArticles() async {
+  List<Article> result;
+  final response = await http
+      .get("https://newsapi.org/v2/everything?apiKey=$apiKey&q=bitcoin");
+
+  final jsonData = jsonDecode(response.body);
+
+  if (response.statusCode == 200) {
+    result =
+        (jsonData['articles'] as List).map((e) => Article.fromJson(e)).toList();
+    return result;
+  } else {
+    throw Exception('Failed to load news');
   }
 }
